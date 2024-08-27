be8e267b93c3ff37260d11cc53886c36a019a183
Rila <rilanajjakha@mail.ugm.ac.id>
2024-08-27 21:53:34 +0700

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+    implementation(kotlin("script-runtime"))
     implementation("net.objecthunter:exp4j:0.4.8")
 
 }
